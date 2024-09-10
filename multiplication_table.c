13b2907af116df3c4591319d16c30e1f0131d002
Ganesha_G_Rao <130823089+K-Ganesh-Rao@users.noreply.github.com>
2024-09-10 19:13:42 +0530

    Update multiplication_table.c

--- multiplication_table.c
+++ multiplication_table.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
 
+int main(void){
+    int i;
+    int mul;
+    int num;
+    
+    printf("Entera number");
+    scanf("%d", &num);
+
+    for(i=0; i<=10; i++){
+        mul = num *i;
+        printf("%d * %d = %d\n",num , i , mul);
+
+    }
+
+return 0;
+
+}
