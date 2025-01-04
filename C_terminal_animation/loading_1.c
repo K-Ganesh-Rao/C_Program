171ebbf140e878d2e166641af88d81291ba8c97e
Ganesha_G_Rao <130823089+K-Ganesh-Rao@users.noreply.github.com>
2025-01-04 16:20:09 +0530

    Create loading_1.c

---
+++ C_terminal_animation/loading_1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h> // For usleep
+
+int main()
+{
+
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	while(1) {
+		printf("              \\\r");
+		fflush(stdout);  // Ensures the output is immediately written to the console
+		usleep(100000);  // 100 milliseconds delay
+
+		printf("              -\r");
+		fflush(stdout);
+		usleep(100000);
+
+		printf("              /\r");
+		fflush(stdout);   
+		usleep(100000);
+
+		printf("              |\r");
+		fflush(stdout);
+		usleep(100000);
+	}
+	return 0;
+}
